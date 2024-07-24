056d8c367736ca6dcd26c172f07042d18eb78370
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-07-24 10:02:47 -0300

    Criando Document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Sujeito Pizza</h1>
-    </div>
+    <>
+      <Head>
+        <title>SujeitoPizza - Faça seu Login</title>
+      </Head>
+      <div>
+        <h1>Sujeito Pizza</h1>
+      </div>
+    </>
   );
 }
