8bf66f5a435d5a4e23c2b94a9e5138728a0bee66
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-07-25 10:29:02 -0300

    Página de Cadastro

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -2,35 +2,36 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-import logo from "../../public/logo.svg";
-import styles from "../styles/home.module.scss";
+import logo from "../../../public/logo.svg";
+import styles from "../../styles/home.module.scss";
 
-import { Input } from "../components/ui/Input";
-import { Button } from "../components/ui/Button";
+import { Input } from "../../components/ui/Input";
+import { Button } from "../../components/ui/Button";
 
-export default function SignUp() {
+export default function Home() {
   return (
     <>
       <Head>
-        <title>SujeitoPizza - Faça seu Login</title>
+        <title>SujeitoPizza - Faça seu Cadastro</title>
       </Head>
       <main className={styles.containerCenter}>
         <Image src={logo} alt="Logo Sujeito Pizzaria" />
 
         <div className={styles.login}>
+          <h1>Criando sua Conta</h1>
+
           <form>
+            <Input type="text" placeholder="Digite seu Nome" />
             <Input type="text" placeholder="Digite seu E-mail" />
             <Input type="password" placeholder="Digite seu Senha" />
 
             <Button type="button" loading={false}>
-              Acessar
+              Cadastrar
             </Button>
           </form>
 
-          <Link href="/signup">
-            <span className={styles.text}>
-              Não possui uma conta? Cadastre-se
-            </span>
+          <Link href="/">
+            <span className={styles.text}>Já possui uma conta? Faça Login</span>
           </Link>
         </div>
       </main>
