e921649a31987b24605f8c58ae4777f3d709b1aa
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-08-05 11:35:14 -0300

    Configurando Modal Personalizado

---
+++ src/components/ModalOrder/index.tsx
@@ -0,0 +1,7 @@
+export function ModalOrder() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -9,6 +9,9 @@ import { FiRefreshCcw } from "react-icons/fi";
 import { canSSRAuth } from "../../utils/canSSRAuth";
 import { setupAPIClient } from "../../services/api";
 
+import Modal from "react-modal";
+import { ModalOrder } from "../../components/ModalOrder";
+
 type OrderProps = {
   id: string;
   table: number;
@@ -21,10 +24,49 @@ interface HomeProps {
   orders: OrderProps[];
 }
 
+export type OrderItemProps = {
+  id: string;
+  amount: number;
+  order_id: string;
+  product_id: string;
+  product: {
+    id: string;
+    name: string;
+    description: string;
+    price: string;
+    banner: string;
+  };
+  order: {
+    id: string;
+    table: string | number;
+    status: boolean;
+    name: string | null;
+  };
+};
+
 export default function Dashboard({ orders }: HomeProps) {
   const [orderList, setOrderList] = useState(orders || []);
 
-  function handleOpenModalView(id: string) {}
+  const [modalItem, setModalItem] = useState<OrderItemProps[]>();
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function handleCloseModal() {
+    setModalVisible(false);
+  }
+
+  async function handleOpenModalView(id: string) {
+    const apiClient = setupAPIClient();
+    const response = await apiClient.get("/order/detail", {
+      params: {
+        order_id: id,
+      },
+    });
+
+    setModalItem(response.data);
+    setModalVisible(true);
+  }
+
+  Modal.setAppElement("#__next");
 
   return (
     <>
@@ -53,6 +95,8 @@ export default function Dashboard({ orders }: HomeProps) {
             ))}
           </article>
         </main>
+
+        {modalVisible && <ModalOrder />}
       </div>
     </>
   );
